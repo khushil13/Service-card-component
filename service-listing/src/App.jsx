2edee9d72dea804d25c5cb22360b39d0ae0941b0
TIGER KILLER <choprakhushil13@gmail.com>
2025-02-24 16:07:53 +0530

    updated

--- service-listing/src/App.jsx
+++ service-listing/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import serviceCard from './components/ServiceCard.jsx'
-function App() {
-  const [count, setCount] = useState(0)
- 
+import ServiceCard from "./components/ServiceCard";
+
+const App = () => {
+  const services = [
+    {
+      title: "Web deployement",
+      provider: "James bond",
+      description: "this is about how to do service deployement",
+    },
+    {
+      title: "Web deployement",
+      provider: "James bond",
+      description: "this is about how to do service deployement",
+    },
+    {
+      title: "Web deployement",
+      provider: "James bond",
+      description: "this is about how to do service deployement",
+    },
+  ];
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1>Web deployment</h1>
+      <div className="services">
+        {services.map((service, index) => (
+          <ServiceCard key={index} service={service} />
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- service-listing/src/components/ServiceCard.jsx
+++ service-listing/src/components/ServiceCard.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 
-const ServiceCard = () => {
-    return(
-        <div className="card">
-        <h1>Services</h1>
-        <div>
-            <h2>Frontend-Development</h2>
-            <p>Helps in developing frontend of applications.</p>
+const ServiceCard = ({ service }) => {
+    return (
+        <div className="service-card">
+        <h2>Web-Development</h2>
+        <h4>Tony Stark</h4>
+        <p>This is about service.</p>
         </div>
-        <div>
-            <h2>Backend-Development</h2>
-            <p>Helps in developing backend of applications.</p>
-        </div>
-    )
+    );
 }
+
 export default ServiceCard;
