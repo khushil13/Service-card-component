54c15faa936a61347f526a75f4acc81600e3fb00
TIGER KILLER <choprakhushil13@gmail.com>
2025-02-24 15:55:04 +0530

    updated

---
+++ service-listing/src/components/ServiceCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ServiceCard = () => {
+    return(
+        <div className="card">
+        <h1>Services</h1>
+        <div>
+            <h2>Frontend-Development</h2>
+            <p>Helps in developing frontend of applications.</p>
+        </div>
+        <div>
+            <h2>Backend-Development</h2>
+            <p>Helps in developing backend of applications.</p>
+        </div>
+    )
+}
+export default ServiceCard;
